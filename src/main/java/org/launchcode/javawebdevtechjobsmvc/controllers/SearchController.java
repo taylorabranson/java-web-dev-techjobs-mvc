fed1213b414b1b72cdae97674ccdbb20629a37d0
Taylor Branson <taylor.a.branson@gmail.com>
2020-10-02 18:04:20 -0500

    removed TODO comment

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -23,8 +23,6 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
-
     @RequestMapping(value = "results")
     public String searchResults(Model model,
                                 @RequestParam(required = false) String searchType,
